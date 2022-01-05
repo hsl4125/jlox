8b99127ee56f8642961403afbf0c0b4fce62746b
hsl4125 <qlhuangrui@gmail.com>
2022-01-05 11:44:25 +0800

    add print and expr statement

--- src/com/lox/Interpreter.java
+++ src/com/lox/Interpreter.java
@@ -1,11 +1,16 @@
 package com.lox;
 
-public class Interpreter implements Expr.Visitor<Object>{
+import java.util.List;
 
-    void interpret(Expr expression) {
+public class Interpreter implements Expr.Visitor<Object>,
+        Stmt.Visitor<Void> {
+
+    void interpret(List<Stmt> statements) {
+
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError e) {
             Lox.runtimeError(e);
         }
@@ -134,5 +139,22 @@ public class Interpreter implements Expr.Visitor<Object>{
 
     private Object evaluate(Expr expr) {
        return expr.accept(this);
+    }
+
+    private void execute(Stmt statement){
+        statement.accept(this);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 }
